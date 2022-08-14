4661c19afe889f45192e230d8b6bdfb075b87065
Lauren <elsee388@protonmail.com>
2022-08-14 15:58:29 -0400

    created landing page, and added animation

---
+++ src/components/landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div className="landing">
+    <div className="wrapper">
+      <h1 className="enter">Click to Enter</h1>
+    </div>
+    </div>
+    )
+}
+
+export default Landing;
