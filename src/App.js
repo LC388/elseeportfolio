fdd70c27e558af82b17ca5d4f1f66c7c69043687
Lauren <elsee388@protonmail.com>
2022-10-05 21:44:38 -0400

    built and basic style to Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import Landing from './components/landing';
+import Landing from './components/Landing';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
 <>
-  <Landing/>
+  {/*<Landing/>*/}
+  <Navbar/>
 </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import tempLogo from "../images/tempLogo.png";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <img src={tempLogo} className="navbar--logo" alt="logo"/>
+      <ul className="navbar--menu">
+          <li className="navbar--item">About</li>
+          <li className="navbar--item">Contact</li>
+          <li className="navbar--item">Facts</li>
+        </ul>
+        
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
