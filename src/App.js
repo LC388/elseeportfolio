5e894b0696b0146c4f7ff223b74f938f53247055
Lauren <elsee388@protonmail.com>
2022-10-10 20:19:32 -0400

    footer added and styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
 import './App.css';
 import Landing from './components/Landing';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
 <>
   {/*<Landing/>*/}
   <Navbar/>
+  <Home  />
+  
+  <Footer/>
+
 </>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import github from '../images/github.png'
+import linkedin from '../images/linkedin.png'
+import email from '../images/email.png'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer--container'>
+    <img src={github} alt="github logo" className='footer--icon'/>
+    <img src={linkedin} alt="linkedin logo" className='footer--icon'/>
+    <img src={email} alt="email logo" className='footer--icon'/>
+    </div>  
   )
 }
 
